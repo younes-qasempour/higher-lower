c9c60b44be3fccbb8414eb78f85b7a42f2b51619
younes_qasempour <qasempouryounes@gmail.com>
2025-03-08 19:44:48 +0330

    Finished

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask
+import random
 
+random_number = random.randint(1, 9)
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return ('<h1>Guess a number between 0 and 9</h1>'
+            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">')
+
+@app.route('/number/<int:number>')
+def checks(number):
+    if number == random_number:
+        return ('<h1 style="color:green">You found me!</h1>'
+                '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">')
+
+    elif number > random_number:
+        return ('<h1 style="color:purple">Too high try again!</h1>'
+                '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">')
+    elif number < random_number:
+        return ('<h1 style="color:red">Too low try again!</h1>'
+                '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
